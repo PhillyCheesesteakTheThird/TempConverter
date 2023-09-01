cb15397e6a5ccd643eb3db55d0e432ec5e782a24
Jadan Kerr <badi1863@gmail.com>
2023-09-01 17:24:19 -0600

    Fixed main I think

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int value;
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Do you want to convert from \nA) Celsius > Fahrenheit \nB) Fahrenheit > Celsius ");
+        Scanner myObj = new Scanner(System.in);
+        TempConvert tempConvert = new TempConvert();
+        //Prompts the user for a temperature conversion direction
+        System.out.println("Which way are you converting?");
+        System.out.println("A) Celsius > Fahrenheit \nB) Fahrenheit > Celsius");
 
-        String selection = myObj.nextLine();  // Read user input
-        if(selection.equals("A")  || selection.equals("a")) {
-            System.out.println("Enter the Temperature(C) to be converted");
-            value = myObj.nextInt();
-            System.out.println();
+        String response = myObj.nextLine();
+        String typeSelection;
+        double inputNum;
+        //If user response A, the program uses the C>F conversion. Vice versa if B
+        if (response.equalsIgnoreCase("A")){
+            typeSelection = "A";
+            System.out.println("Enter the value (C) to be converted:");
+
+
         }
         else {
-            System.out.println("Enter the Temperature(F) to be converted");
-            value = myObj.nextInt();
-            System.out.println( (selection * 5/9) + 32);
+            typeSelection = "B";
+            System.out.println("Enter the value (F) to be converted:");
 
 
         }
+        inputNum = Double.parseDouble(myObj.nextLine());
+        //Print output
+        System.out.println(tempConvert.convert(inputNum, typeSelection));
+
     }
 }
-
