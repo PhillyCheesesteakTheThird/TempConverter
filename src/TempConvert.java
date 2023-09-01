e301d106e9eca3362193c40eecc8a9e8b01653a6
Jadan Kerr <badi1863@gmail.com>
2023-09-01 17:16:32 -0600

    Added tempconvert class

---
+++ src/TempConvert.java
@@ -0,0 +1,16 @@
+public class TempConvert {
+    public double convert(double inputInt, String typeSelection) {
+        double output;
+        //Uses the correct formula according to User selection
+        if (typeSelection.equals("A")) {
+            output = (inputInt * 9 / 5) + 32;
+        } else {
+            output = (inputInt * 5 / 9) - 32;
+
+        }
+        return output;
+    }
+
+
+}
+
